00f75a6a5e361a8db85fe33c199092dd1aedd5a4
Julian Bernal <bernaljuliane@gmail.com>
2024-03-24 21:09:38 -0300

    Se crea repositorio con sus respectivos packages

---
+++ src/main/kotlin/ar/edu/unq/dapp/GrupoF/backenddappapl/BackendDappAplApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.dapp.GrupoF.backenddappapl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendDappAplApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendDappAplApplication>(*args)
+}
---
+++ src/test/kotlin/ar/edu/unq/dapp/GrupoF/backenddappapl/BackendDappAplApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.edu.unq.dapp.GrupoF.backenddappapl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendDappAplApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
